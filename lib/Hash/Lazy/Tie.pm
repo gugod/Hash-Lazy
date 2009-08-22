00ba987c8977904a4cc97796e12b93cc984e6742
Kang-min Liu <gugod@gugod.org>
2009-08-22 14:29:44 +0800

    implement several basic hash operation.

--- lib/Hash/Lazy/Tie.pm
+++ lib/Hash/Lazy/Tie.pm
@@ -3,13 +3,12 @@ use strict;
 use warnings;
 use Carp qw(confess);
 use parent 'Tie::Hash';
+use self;
 
 use constant USAGE => 'my %h; tie %h, "Hash::Lazy::Tie", $builder, \%h;';
 
 sub TIEHASH {
-    my $class = shift;
-    my $builder = shift;
-    my $tied = shift;
+    my ($builder, $tied) = @args;
 
     confess "The use of Hash::Lazy::Tie requires passing the hash variable that it's tying to. Usage: " . USAGE
         unless defined($tied);
@@ -18,25 +17,34 @@ sub TIEHASH {
         builder => $builder,
         tied     => $tied,
         storage  => {}
-    }, __PACKAGE__;
+    }, $self;
 }
 
 sub STORE {
-    my $self = shift;
-    my $key = shift;
-    my $value = shift;
+    my ($key, $value) = @args;
     $self->{storage}{$key} = $value;
 }
 
 sub FETCH {
-    my $self = shift;
-    my $key = shift;
+    my ($key) = @args;
 
     return $self->{storage}{$key} if exists $self->{storage}{$key};
 
     my $ret = $self->{builder}->($self->{tied}, $key);
     $self->{storage}{$key} = $ret unless exists $self->{storage}{$key};
     return $self->{storage}{$key};
+}
+
+sub DELETE {
+    my ($key) = @args;
+
+    delete $self->{storage}{$key};
+}
+
+sub CLEAR {
+    for my $key (keys %{$self->{storage}}) {
+        delete $self->{storage}{$key};
+    }
 }
 
 1;
